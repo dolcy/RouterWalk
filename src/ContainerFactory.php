d15a69964e9df817923d43922db7ad6930bcc278
Shaughn Dolcy <sdolcy@gmail.com>
2019-11-12 16:04:35 -0500

    Remove routing provider service; use Route.php

--- src/ContainerFactory.php
+++ src/ContainerFactory.php
@@ -5,17 +5,18 @@ declare(strict_types=1);
 namespace RouterApp;
 
 use League\Container\Container;
+use League\Container\ReflectionContainer;
 use RouterApp\ServiceProvider\DatabaseServiceProvider;
-use RouterApp\ServiceProvider\RouteServiceProvider;
 
 class ContainerFactory
 {
     public static function start()
     {
+        // Register the reflection container for autowiring
         $container = new Container();
+
         // Add Services
         $container->addServiceProvider(DatabaseServiceProvider::class);
-        $container->addServiceProvider(RouteServiceProvider::class);
 
         return $container;
     }
